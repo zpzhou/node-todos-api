27041d0a2a3a406a0739d4a772c25f3afd740c5d
zpzhou <zpzhou@sfu.ca>
2018-08-18 13:09:17 -0700

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -30,6 +31,27 @@ app.get('/todos', (req, res) => {
         });
     }, (err) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send({
+                message: `${id} not found`
+            });
+        }
+        res.status(200).send({
+            todo: todo
+        });
+    }).catch((err) => {
+        res.status(400).send({
+            message: 'foo'
+        });
     });
 });
 
