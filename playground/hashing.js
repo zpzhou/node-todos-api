5d21485848d9803e50f62ec89dde9d911d26eb32
zpzhou <zpzhou@sfu.ca>
2018-08-19 16:34:45 -0700

    hash user password with salt before saving to db

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,22 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123abc!'
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+hashedPassword = '$2a$10$5RlmiDkwABdokoSLiZRVoef/DQsnq2a7jUybkIYR26w3X5PuJV4Tq';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+/*var data = {
     id: 10
 };
 
@@ -10,7 +25,7 @@ console.log(token);
 
 var decoded = jwt.verify(token, 'abc123');
 console.log(decoded);
-
+*/
 /*var message = 'I am user number 3';
 var hash = SHA256(message).toString();
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -72,6 +73,19 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                user.save();
+            });
+        });
+    }
+    next();
+})
 
 var User = mongoose.model('User', UserSchema);
 
